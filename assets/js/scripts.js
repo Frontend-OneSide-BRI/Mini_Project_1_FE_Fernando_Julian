04df6216f75c58283ade322e504cefd6d6de521f
Fernando Julian <nandokun69@gmail.com>
2023-05-31 21:11:18 +0700

    add feature category on gallery.html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+const urlImages = "https://source.unsplash.com/600x600/?";
+
 // get value from input
 function search() {
   var input = document.getElementById("searchInput").value;
@@ -6,20 +8,69 @@ function search() {
   // replace all img src with input value
   var img = document.querySelectorAll("#gambar");
   for (var i = 0; i < img.length; i++) {
-    img[i].src = "https://source.unsplash.com/600x600/?" + input + "&sig=" + i;
+    img[i].src = urlImages + input + "&sig=" + i;
     console.log(img[i].src);
   }
 
   // set condition if input value is empty
   if (input == "") {
     for (var i = 0; i < img.length; i++) {
-      img[i].src = "https://source.unsplash.com/600x600/?flower&sig=" + i;
+      img[i].src = urlImages + i;
       console.log(img[i].src);
     }
   }
 }
 
-function categoryButton() {
-  var category = document.getElementById("flower").innerText.toLowerCase();
-  console.log(category);
+const showCategory = document.getElementById("showCategory");
+
+function categoryImages(params1) {
+  showCategory.textContent = "";
+
+  for (let i = 0; i < 2; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.style.width = "28rem";
+
+    const contaner = document.createElement("div");
+    contaner.classList.add("image-container", "overflow-hidden");
+
+    const img = document.createElement("img");
+    img.classList.add("img-fluid");
+    img.src = urlImages + params1 + "&sig=" + i;
+
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+
+    contaner.appendChild(img);
+    card.appendChild(contaner);
+    card.appendChild(overlay);
+    showCategory.appendChild(card);
+  }
 }
+
+const buttonflower = document.getElementById("flower");
+buttonflower.addEventListener("click", function () {
+  const params1 = "flower";
+  categoryImages(params1);
+});
+
+const buttonSoccer = document.getElementById("soccer");
+buttonSoccer.addEventListener("click", function () {
+  const params1 = "soccer";
+  categoryImages(params1);
+});
+const buttonComputer = document.getElementById("computer");
+buttonComputer.addEventListener("click", function () {
+  const params1 = "computer";
+  categoryImages(params1);
+});
+const buttonLaptop = document.getElementById("laptop");
+buttonLaptop.addEventListener("click", function () {
+  const params1 = "laptop";
+  categoryImages(params1);
+});
+const buttonGame = document.getElementById("game");
+buttonGame.addEventListener("click", function () {
+  const params1 = "game";
+  categoryImages(params1);
+});
